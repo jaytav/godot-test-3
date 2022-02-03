19611d9309971f2a379d5a3a314158df9dff6657
Jayden Tavares <jayden.tavares@hotmail.com>
2022-02-03 19:44:59 +1100

    create main script, world, world gui and entity

---
+++ main.gd
@@ -0,0 +1,13 @@
+tool
+extends Node
+
+export(Array, PackedScene) var _initial_scenes
+
+
+func _ready():
+    for scene in _initial_scenes:
+        var instance = scene.instance()
+        
+        if !has_node(instance.name):
+            add_child(instance)
+            instance.set_owner(get_tree().edited_scene_root)
