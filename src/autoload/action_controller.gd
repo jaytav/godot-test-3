ca5c8e0383988131cf73a8addaa03573faefb8e5
Jayden Tavares <jayden.tavares@hotmail.com>
2022-02-13 22:47:25 +1100

    update astar movement for other characters

--- src/autoload/action_controller.gd
+++ src/autoload/action_controller.gd
@@ -12,21 +12,28 @@ var astar_movement: AStar2D = AStar2D.new()
 func init() -> void:
     tile_map_action = GroupController.get("TileMaps").get("Action")
     tile_map_floor = GroupController.get("TileMaps").get("Floor")
+
     refresh_astar_movement()
 
 
 func refresh_astar_movement() -> void:
     astar_movement.clear()
+    var character_cells: Array
+
+    for character in GroupController.get("Characters").all():
+        character_cells.append(tile_map_action.world_to_map(character.position))
 
     for cell in tile_map_floor.get_used_cells():
         astar_movement.add_point(get_vector_point_index(cell), cell)
 
     for point in astar_movement.get_points():
         var cell: Vector2 = astar_movement.get_point_position(point)
-        
+
         var relative_cells: Array = [
-            cell + Vector2.RIGHT,
+            cell + Vector2.UP,
             cell + Vector2.DOWN,
+            cell + Vector2.LEFT,
+            cell + Vector2.RIGHT,
         ]
 
         for relative_cell in relative_cells:
@@ -35,7 +42,10 @@ func refresh_astar_movement() -> void:
             if !astar_movement.has_point(relative_cell_point):
                 continue
 
-            astar_movement.connect_points(point, relative_cell_point)
+            if character_cells.has(relative_cell):
+                continue
+
+            astar_movement.connect_points(point, relative_cell_point, false)
 
 
 func get_vector_point_index(cell: Vector2) -> int:
