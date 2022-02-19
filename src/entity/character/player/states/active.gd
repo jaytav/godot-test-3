631d8ba52eb1cc6430d05e8f7850e03e5b8c670c
Jayden Tavares <jayden.tavares@hotmail.com>
2022-02-19 22:42:30 +1100

    add scroll actions function

--- src/entity/character/player/states/active.gd
+++ src/entity/character/player/states/active.gd
@@ -11,4 +11,17 @@ func unhandled_input(event: InputEvent) -> void:
        owner.active_action.do()
     elif event.is_action_pressed("ui_select"):
         owner.end_turn()
-    
+    elif event.is_action_pressed("ui_focus_next"):
+        var active_action: Action = owner.active_action
+        var actions: Array = owner.get_node("Actions").get_children()
+
+        for i in range(actions.size()):
+            if actions[i] == active_action:
+                if actions[i] == actions.back():
+                    owner.active_action = actions.front()
+                else:
+                    owner.active_action = actions[i + 1]
+                break
+
+        owner.active_action.refresh_cells()
+        owner.active_action.draw_cells()
